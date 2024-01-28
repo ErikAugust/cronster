41b90291c0d4c6118962debe55d7f1503e7cd0a2
Erik Johnson <erik@erikaugust.com>
2024-01-28 12:53:59 -0500

    CRN-65: Adds plugin to exclude common files in order to reduce size of deployment

--- src/serverless/tweet-random-cron.ts
+++ src/serverless/tweet-random-cron.ts
@@ -1,6 +1,48 @@
 import { Handler } from 'aws-lambda';
+import { Rettiwt } from 'rettiwt-api';
+import mysql from 'mysql2/promise';
 
 export const handler: Handler = async (event, context) => {
-    console.log('EVENT: \n' + JSON.stringify(event, null, 2));
-    return context.logStreamName;
+
+  try {
+    const twitter = new Rettiwt({ apiKey: process.env.TWITTER_API_KEY });
+    const database = await mysql.createConnection({
+      host: process.env.CRONSTER_DB_HOST,
+      user: process.env.CRONSTER_DB_USERNAME,
+      password: process.env.CRONSTER_DB_PASSWORD,
+      database: process.env.CRONSTER_DB_NAME
+    });
+
+    const results = await database.query('SELECT * FROM Crons WHERE deleted = 0');
+    const cron = getRandomCron(results[0] as any[]);
+  
+    console.log(cron);
+  
+    const title = cron.title;
+    const slug = cron.slug;
+  
+    const tweet = `${title} \n\nhttps://cronster.app/@erik/${slug}`;
+
+    const result = await twitter.tweet.tweet(tweet); 
+    if (!result) {
+      console.error('Tweet failed!');
+      return { message: 'Tweet failed for unknown reason' };
+    }
+    console.log('Tweet succeeded...');
+    const { list } = await twitter.user.timeline('211721484');
+    console.log(list[0]);
+
+    return {
+      tweet
+    }
+  } catch (error: any) {
+    console.error(error);
+    return {
+      message: error.message
+    }
+  }
 };
+
+function getRandomCron(crons: any[]) {
+  return crons[Math.floor(Math.random() * crons.length)];
+}
